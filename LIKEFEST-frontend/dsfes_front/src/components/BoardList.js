e030e2672c1db5fb6af50f1fd65747eaf4cde628
Chanran33 <k55416325@gmail.com>
2022-05-17 22:00:30 +0900

    수정이와 오붓한 데이트

--- LIKEFEST-frontend/dsfes_front/src/components/BoardList.js
+++ LIKEFEST-frontend/dsfes_front/src/components/BoardList.js
@@ -3,34 +3,32 @@ import BoardItem from "./BoardItem";
 import styles from "../css/Board.module.css";
 
 const BoardList = ({ texts }) => {
-  //console.log(texts);
   const [datalist, setDatalist] = useState([]);
 
-  // 추가 데이터를 로드하는지 아닌지를 담기위한 state
-  const [fetching, setFetching] = useState(false);
-
   //자를 길이
-  const DATA_SIZE = 20;
+  const DATA_SIZE = 10;
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
   const fetchMoreData = useCallback(() => {
-    setFetching(true);
+    //console.log("fetchMoreData");
     setDatalist(texts.slice(-DATA_SIZE * count));
-    setFetching(false);
-  }, [count]);
+  }, [count,texts]);
 
-  const handleScroll = () => {
+  const handleScroll =  useCallback(()=>{
+    //console.log("handleScroll");
     const scrollHeight = document.documentElement.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
     const clientHeight = document.documentElement.clientHeight;
 
-    if (scrollTop + clientHeight + 1 >= scrollHeight && fetching === false) {
-      fetchMoreData();
-      setCount(count + 1);
-      //console.log(count);
+    if(datalist.length < texts.length){
+      if (scrollTop + clientHeight + 200 >= scrollHeight) {
+        fetchMoreData();
+        setCount((count) => count + 1);
+      }
     }
-  };
+  },[datalist.length, fetchMoreData, texts.length]);
+    
 
   useEffect(() => {
     setDatalist(texts.slice(-DATA_SIZE));
@@ -44,7 +42,7 @@ const BoardList = ({ texts }) => {
       // scroll event listener 해제
       window.removeEventListener("scroll", handleScroll);
     };
-  });
+  },[handleScroll]);
 
   return (
     <div className={styles.scrollcontent}>
